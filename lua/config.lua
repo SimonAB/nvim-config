SimonAB/nvim-config

commit 64a51c9562f0560531395d92777d914e77ba2ba0

feat: move spell dictionaries to private directory for privacy
- Move spell/ directory to private/spell/ to ensure personal dictionaries stay private
- Configure spellfile option to use private location (private/spell/*.add)
- Update .gitignore to reflect new privacy-focused approach
- All 284 English + 22 French custom words now kept private while syncing
- Spell checking functionality fully preserved and tested

Privacy benefits:
- Personal names, research projects, institution names no longer public
- Religious/biblical terms kept private
- Academic terms remain available but not exposed in public repo
- Full functionality maintained with complete privacy

lua/config.lua
@@ -55,6 +55,10 @@ opt.exrc = false -- Disable exrc file
 opt.spelllang = "en_gb" -- Set British English spelling
 opt.spell = true -- Enable spell checking by default
 
+-- Configure spell directories (private location for privacy)
+local config_dir = vim.fn.stdpath('config')
+opt.spellfile = config_dir .. '/private/spell/en.utf-8.add,' .. config_dir .. '/private/spell/fr.utf-8.add'
+
 -- Terminal colours (enables true colour support)
 opt.termguicolors = true
 

.gitignore
@@ -1,6 +1,4 @@
-# Compiled spell files (but allow custom dictionaries to sync)
-spell/*.spl
-spell/*.sug
+# Personal spell dictionaries (moved to private/ for privacy)
 
 # Private keys and sensitive data
 private/

commit 8f1d036fdf5aecee37cc2b04a07de746c0ed5c64

fix: correct French spell language switching
Fixes multiple issues introduced in commit 64a51c9 that prevented French
spell checking from working properly:

1. Spell cache clearing: Language switches now disable/re-enable spell
   checking to force Neovim to reload the spell system (main issue)
2. Runtime path: Added private/ directory to runtimepath so Neovim can
   find the French base dictionary (fr.utf-8.spl)
3. Configuration order: Reordered spell setup to configure paths before
   enabling spell checking
4. Dynamic spellfile: Language switches now update spellfile option to
   point to the correct dictionary (.add file)
5. User feedback: Added notifications when switching languages

The root cause was that Neovim caches the spell system on initialisation.
Simply changing spelllang wasn't sufficient - spell checking must be
disabled and re-enabled to force a complete reload.

Tested: French and English spell checking now work correctly with
<leader>Ysf and <leader>Yse keymaps.

lua/config.lua
@@ -52,12 +52,14 @@ opt.modeline = false -- Disable modeline
 opt.exrc = false -- Disable exrc file
 
 -- Language and spell checking
+-- Configure spell directories FIRST (before enabling spell checking)
+local config_dir = vim.fn.stdpath('config')
+-- Add private directory to runtimepath so Neovim can find base spell files
+vim.opt.runtimepath:prepend(config_dir .. '/private')
+opt.spellfile = config_dir .. '/private/spell/en.utf-8.add' -- Default to English, switches with language
+-- NOW enable spell checking after paths are configured
 opt.spelllang = "en_gb" -- Set British English spelling
 opt.spell = true -- Enable spell checking by default
-
--- Configure spell directories (private location for privacy)
-local config_dir = vim.fn.stdpath('config')
-opt.spellfile = config_dir .. '/private/spell/en.utf-8.add,' .. config_dir .. '/private/spell/fr.utf-8.add'
 
 -- Terminal colours (enables true colour support)
 opt.termguicolors = true

lua/keymaps.lua
@@ -900,12 +900,20 @@ end
 -- Toggle options (these were missing from the moved keymaps)
 map("n", "<leader>Ys", "<cmd>set spell!<CR>", { desc = "Toggle Spell Check" })
 map("n", "<leader>Yse", function()
+  local config_dir = vim.fn.stdpath('config')
+  vim.cmd("set nospell")  -- Disable first to clear spell cache
+  vim.opt.spellfile = config_dir .. '/private/spell/en.utf-8.add'
   vim.cmd("set spelllang=en_gb")
-  vim.cmd("set spell")
+  vim.cmd("set spell")  -- Re-enable with new language
+  vim.notify("Spell language: English (British)", vim.log.levels.INFO)
 end, { desc = "Set spell language to English (British)" })
 map("n", "<leader>Ysf", function()
+  local config_dir = vim.fn.stdpath('config')
+  vim.cmd("set nospell")  -- Disable first to clear spell cache
+  vim.opt.spellfile = config_dir .. '/private/spell/fr.utf-8.add'
   vim.cmd("set spelllang=fr")
-  vim.cmd("set spell")
+  vim.cmd("set spell")  -- Re-enable with new language
+  vim.notify("Spell language: French", vim.log.levels.INFO)
 end, { desc = "Set spell language to French" })
 
 -- Split operations (removing duplicate)